8c0872659b682ed592389b14378470f82682ffc3
msbizrise-arch <msbizrise@gmail.com>
2026-03-05 16:59:40 +0530

    Update plans_db.py

--- Extractor/core/mongo/plans_db.py
+++ Extractor/core/mongo/plans_db.py
@@ -1,17 +1,56 @@
-from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URL
+import datetime
+import pytz
 
-mongo = MongoCli(MONGO_URL)
-db = mongo.premium.premium_db
+# Initialize MongoDB client
+mongo = AsyncIOMotorClient(MONGO_URL)
+db = mongo.pw_extractor.premium_users
 
 async def add_premium(user_id, expire_date):
-    await db.update_one({"_id": user_id}, {"$set": {"expire_date": expire_date}}, upsert=True)
+    """Add or update premium user"""
+    await db.update_one(
+        {"_id": user_id},
+        {"$set": {"expire_date": expire_date, "joined": datetime.datetime.now(pytz.utc)}},
+        upsert=True
+    )
 
 async def remove_premium(user_id):
+    """Remove premium user"""
     await db.delete_one({"_id": user_id})
 
 async def check_premium(user_id):
-    return await db.find_one({"_id": user_id})
+    """Check if user has premium"""
+    data = await db.find_one({"_id": user_id})
+    if data and "expire_date" in data:
+        # Check if expired
+        now = datetime.datetime.now(pytz.utc)
+        if data["expire_date"] < now:
+            await remove_premium(user_id)
+            return None
+    return data
 
 async def premium_users():
-    return [data["_id"] async for data in db.find()]
+    """Get list of all premium user IDs"""
+    users = []
+    async for data in db.find():
+        # Check if expired
+        if "expire_date" in data:
+            now = datetime.datetime.now(pytz.utc)
+            if data["expire_date"] > now:
+                users.append(data["_id"])
+            else:
+                await remove_premium(data["_id"])
+        else:
+            users.append(data["_id"])
+    return users
+
+async def get_premium_count():
+    """Get total premium users count"""
+    count = 0
+    async for data in db.find():
+        if "expire_date" in data:
+            now = datetime.datetime.now(pytz.utc)
+            if data["expire_date"] > now:
+                count += 1
+    return count
