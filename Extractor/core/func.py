f7e4b6f420272399d423685c26140c199e855f82
msbizrise-arch <msbizrise@gmail.com>
2026-03-05 15:33:34 +0530

    Add files via upload

---
+++ Extractor/core/func.py
@@ -0,0 +1,60 @@
+from config import CHANNEL_ID, SUDO_USERS, OWNER_ID
+from Extractor.core import script
+from pyrogram.errors import UserNotParticipant
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from Extractor.core.mongo.plans_db import premium_users
+
+
+async def chk_user(user_id):
+    """Check if user has premium access. Returns 0 if allowed, 1 if not."""
+    users = await premium_users()
+    if user_id in users:
+        return 0
+    if user_id in SUDO_USERS:
+        return 0
+    if user_id == OWNER_ID:
+        return 0
+    return 1
+
+
+async def subscribe(client, message):
+    """Check if user is subscribed to the channel. Returns 0 if ok, 1 if not."""
+    if not CHANNEL_ID or CHANNEL_ID == 0:
+        return 0
+    try:
+        user = await client.get_chat_member(CHANNEL_ID, message.from_user.id)
+        if user.status in ["kicked", "left"]:
+            raise UserNotParticipant
+        return 0
+    except UserNotParticipant:
+        try:
+            url = await client.export_chat_invite_link(CHANNEL_ID)
+        except Exception:
+            return 0
+        await message.reply_photo(
+            "https://graph.org/file/b7a933f423c153f866699.jpg",
+            caption=script.FORCE_MSG.format(message.from_user.mention),
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Join Channel", url=url)]]
+            ),
+        )
+        return 1
+    except Exception:
+        return 0
+
+
+async def get_seconds(time_string):
+    """Convert time string like '30days' to seconds."""
+    value = int("".join(filter(str.isdigit, time_string)))
+    unit = "".join(filter(str.isalpha, time_string)).lower()
+    if unit.startswith("day"):
+        return value * 86400
+    if unit.startswith("hour"):
+        return value * 3600
+    if unit.startswith("min"):
+        return value * 60
+    if unit.startswith("month"):
+        return value * 86400 * 30
+    if unit.startswith("year"):
+        return value * 86400 * 365
+    return 0
---
+++ Extractor/core/script.py
@@ -0,0 +1,12 @@
+IMG = ["https://graph.org/file/dbd48ba7093582ab20063.jpg"]
+
+START_TXT = """**Hello** {},
+
+I'm **PW Extractor Bot**!
+Extract Physics Wallah course videos (M3U8) & notes easily.
+
+Click **Physics Wallah** below to get started!"""
+
+FORCE_MSG = """**Hey** {},
+
+You must join my updates channel first to use this bot!"""
