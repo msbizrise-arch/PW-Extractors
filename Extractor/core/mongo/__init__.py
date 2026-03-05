1ef45b67b16076677782fd81a84dd5d9a67bc718
msbizrise-arch <msbizrise@gmail.com>
2026-03-05 18:07:31 +0530

    Add files via upload

--- Extractor/core/mongo/__init__.py
+++ Extractor/core/mongo/__init__.py
@@ -1,7 +1,1 @@
-# Extractor/core/mongo/__init__.py
-# This file makes the 'mongo' folder a proper Python package
-# No code needed here unless you want to expose specific names
-
-# Optional: you can do
-# from .plans_db import *
-# ...but better to import directly where needed (as we did in plans.py)
+# MongoDB package initialization
