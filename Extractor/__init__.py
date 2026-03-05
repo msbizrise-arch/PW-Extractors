51ce9653b8327ed9ffb36eac6ee4eaa6b43be90f
msbizrise-arch <msbizrise@gmail.com>
2026-03-05 15:33:03 +0530

    Add files via upload

---
+++ Extractor/__init__.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+app = Client(
+    "PWExtractor",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    in_memory=True,  # Don't create session file (important for Render)
+)
