fecc356f043e9ce53a603eda9aff8bd7b61eb7f4
msbizrise-arch <msbizrise@gmail.com>
2026-03-05 20:18:46 +0530

    Update __init__.py

--- Extractor/__init__.py
+++ Extractor/__init__.py
@@ -1,14 +1,12 @@
 import nest_asyncio
-nest_asyncio.apply()
+nest_asyncio.apply()  # Fix Render event loop
 
-from pyrogram import Client
+from pyromod import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
 app = Client(
-    "PWExtractorBot",
+    "PWExtractor",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    workers=100,
-    sleep_threshold=120
 )
